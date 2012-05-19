7ee52ef341a39b678dd249aff277acab107b75c6
Jake Lyles <jacob.lyles@gmail.com>
2012-05-19 02:43:23 -0700

    added logins to node-reddit

--- app.js
+++ app.js
@@ -14,99 +14,103 @@
  ********************/
 
 var cli     = require('cli'),
-    http    = require('http'),
-    request = require('request'),
-    argv    = require('optimist').argv,
-    colors  = require('colors');
+		http    = require('http'),
+		request = require('request'),
+		argv    = require('optimist').argv,
+		colors  = require('colors');
 
 var cookie, uh;
 var USER = argv.u || argv.user || argv._[0];
 var PASSWD = argv.p || argv.passwd || argv._[1];
 
 var login = function(opts, cb){
-    var apiOptions = {
-    uri: "http://www.reddit.com/api/login/" + USER,
-    user: USER,
-    headers: {
-      "Host": "www.reddit.com",
-      "Connection": "Keep-Alive",
-      "User-Agent": "logged-in node-reddit bot by /u/jacoblyles"
-    },
-    form:{
-      user: USER,
-      passwd: PASSWD,
-      api_type: "json" 
-    },
-    followAllRedirects: true
-  };
+		var apiOptions = {
+		uri: "http://www.reddit.com/api/login/" + USER,
+		user: USER,
+		headers: {
+			"Host": "www.reddit.com",
+			"Connection": "Keep-Alive",
+			"User-Agent": "logged-in node-reddit bot by /u/jacoblyles"
+		},
+		form:{
+			user: USER,
+			passwd: PASSWD,
+			api_type: "json" 
+		},
+		followAllRedirects: true
+	};
 
-  request.post(apiOptions, function(err, res, body){
-    if (err){
-      console.error(err);
-    }
-    var data = JSON.parse(body);
-    cookie = data.cookie;
-    uh = data.modhash;
-    cb();
-  });
+	request.post(apiOptions, function(err, res, body){
+		if (err){
+			console.error(err);
+		}
+		var data = JSON.parse(body);
+		cookie = data.json.data.cookie;
+		console.log("cookie is", cookie);
+		console.log(body, res.headers);
+		uh = data.modhash;
+		cb();
+	});
 
 };
 
 
 var fetch = function(opts, cb){
-  apiOptions = {
-    uri: 'http://reddit.com/.json',
-    headers: {
-      "Cookie": cookie,
-      "User-Agent": "logged-in node-reddit bot by /u/jacoblyles",
-      "Host": "www.reddit.com",
-    }
-  }
+	apiOptions = {
+		uri: 'http://reddit.com/.json',
+		headers: {
+			"Cookie": cookie,
+			"User-Agent": "logged-in node-reddit bot by /u/jacoblyles",
+			"Host": "www.reddit.com",
+		}
+	}
 
-  request(apiOptions, function (err, res, body) {
-    if (!err && res.statusCode === 200) {
-      var reddit  = JSON.parse(body),
-          stories = reddit.data.children.map(function (s) { 
-                      return s.data; 
-                    });
-      
-      // Descending score
-      stories.sort(function (a, b) { return b.score - a.score; });
+	console.log(apiOptions);
 
-      stories.forEach(function (story) {
-        var row = "",
-          title = story.title.length > 100
-                ? story.title.substr(0, 100) + "..." 
-                : story.title;
+	request(apiOptions, function (err, res, body) {
+		if (!err && res.statusCode === 200) {
+			var reddit  = JSON.parse(body),
+					stories = reddit.data.children.map(function (s) { 
+											return s.data; 
+										});
+			
+			// Descending score
+			stories.sort(function (a, b) { return b.score - a.score; });
 
-        // Build row
-        // [score] [title] [comments] [subreddit]
-        // This sucks
-        row += story.score.toString().green + "\t";
-        row += title.bold
-        row += " (" + story.domain + ")";
-        row += (" /r/" + story.subreddit).cyan;
-        row += "\n\t";
-        row += story.author.grey;     
-        row += " " + (story.num_comments + " comments").italic.yellow;
-        row += "\n";
+			stories.forEach(function (story) {
+				var row = "",
+					title = story.title.length > 100
+								? story.title.substr(0, 100) + "..." 
+								: story.title;
 
-        console.log(row);
-      });
-    }
-  });
+				// Build row
+				// [score] [title] [comments] [subreddit]
+				// This sucks
+				row += story.score.toString().green + "\t";
+				row += title.bold
+				row += " (" + story.domain + ")";
+				row += (" /r/" + story.subreddit).cyan;
+				row += "\n\t";
+				row += story.author.grey;     
+				row += " " + (story.num_comments + " comments").italic.yellow;
+				row += "\n";
+
+				console.log(row);
+			});
+		}
+	});
 }
 
 
 cli.main(function (args, options) {
-  console.log("***********".rainbow);
-  console.log("Node Reddit".cyan);
-  console.log("***********\n\n".rainbow);
-  
-  if (USER && PASSWD){
-    login({}, fetch);
-  } else { 
-    fetch();
-  }
+	console.log("***********".rainbow);
+	console.log("Node Reddit".cyan);
+	console.log("***********\n\n".rainbow);
+	
+	if (USER && PASSWD){
+		login({}, fetch);
+	} else { 
+		fetch();
+	}
 });
 
